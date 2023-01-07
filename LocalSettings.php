db85c28f1ee24797469ad62b63a434d8d3ef9b2f
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2023-01-07 07:52:41 -0500

    Update URL

--- LocalSettings.php
+++ LocalSettings.php
@@ -29,7 +29,7 @@ $wgSitename = "Civable";
 $wgScriptPath = "";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "https://18.221.184.130";
+$wgServer = "https://civable.com/";
 
 ## The URL path to static resources (images, scripts, etc.)
 $wgResourceBasePath = $wgScriptPath;
